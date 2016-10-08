a9290ab9c09149c857ca6920318583cda1263ce0
Julien00859 <julien.castiaux@gmail.com>
2016-10-08 16:31:28 +0200

    cacas de julien

--- defs.h
+++ defs.h
@@ -1,54 +1,41 @@
 #define MEM_KEY 20135
-#define N_CARS 22
 
-//CORE STATUS
-#define WAITING       0
-#define READY         1
-#define ESSAI         2
-#define QUALIFICATION 3
-#define COURSE        4
+// Core status
+#define S_INIT				0
+#define S_READY				1
+#define S_TEST_RACES		2
+#define S_QUALIFYING_RACES	3
+#define S_RACE				4
 
-#define TIMINGS(TYPE, ID) { TYPE##ID }
-#define P1            90
-#define P2            90
-#define P3            60
-#define Q1            18
-#define Q2            15
-#define Q3            12 
+// Times for test races, qualif races and the race
+#define T_T1	90
+#define T_T2	90
+#define T_T3	60
+#define T_Q1	18
+#define T_Q2	15
+#define T_Q3	12
+#define T_RACE	0 // Need value
 
-/*
- * Specs code
- * ==========
- * FROM CARS
- * ---------
- * 0 : Program ready. carCode must be 21 (the last one)
- * 1-3 : The car just left a sector
- * 4 : Car just entered the pitstop
- * 5 : Car just left the pitstop
- * 9 : Car abandoned the race
- * FROM MANAGER
- * ------------
- * 11 : Specify in carCode the next race time or laps
- * 12 : Specify mode in carCode (0: time, 1: laps)
- */
-typedef struct{
-    unsigned int carCode;
-    unsigned int eventCode;
-}f1CarEvent;
+/* time by sectors for a lap */
+typedef struct sector_times {
+	sector_times* previous_lap,
+	sector_times* next_lap,
+	float s1,
+	float s2,
+	float s3
+};
 
-typedef struct{
-    float s1;
-    float s2;
-    float s3;        
-}f1Times;
+/* all info for a pilote */
+typedef struct f1_pilote {
+	unsigned int id, // internaly used id [0; #PILOTES]
+	unsigned int pilote_id, // printable id (pilote number)
+	void* current_lap, // point a sector_times
+	char current_sector // sector [1], sector [2], sector [3]
+	char status, // [D]riving, [P]itstop, [O]ut
+};
 
-typedef struct{
-    unsigned char    writingLock; 
-    unsigned int     carNumber; //Will be given by the main process...
-    unsigned int     racingAbandoned;
-    unsigned int     atPitstop;
-    unsigned int     currentSector;
-    unsigned int     lapsCount; 
-             f1Times timesLap; 
-}f1Car;
-
+/* store the time for each pilote at the end of a race */
+typedef struct stored_time {
+	unsigned int pilote_id,
+	void* first_lap
+};
--- main.c
+++ main.c
@@ -6,129 +6,18 @@
 #include <sys/shm.h>
 #include "defs.h"
 
-
-
+/* Check the return value of a syscall, on fail value print a message to stderr and exit, on success value return the value
+	@param call_return: value return by the syscall
+	@param msg_on_fail: message to print before errno if syscall failed
+	@return: call_return
+	@throws: EXIT_FAILURE
+*/
+int try_sys_call(int call_return, char msg_on_fail[]) {
+	if (call_return >= 0) return call_return;
+	perror(msg_on_fail[]);
+	exit(EXIT_FAILURE);
+}
 
 int main(int argc, char *argv[]){
-   printf("Bienvenue au grand Tournoi de F1!\r\n");
-   printf("Nous preparons le tournoi! \r\n");
-   int mem_ID;
-   void* memoire;
-   int pipes[22][2]; //Ce sont les receivers pour les voitures. Seul main y écrit
-   int mainPipe[2];  //C'est le receiver pour le main. Les voitures y envoient les events
-   int carNums[22] = {44, 6, 5, 7, 3, 33, 19, 77, 11, 27, 26, 55, 14, 22, 9, 12, 20, 30, 8, 21, 31, 94};
-   
-   int mustDie = 0;
-
-   if((mem_ID = shmget(MEM_KEY, N_CARS*sizeof(f1Car), IPC_CREAT | 0660 )) < 0){
-       perror("Une erreur est survenue durant l'alignement des voitures: ");
-       exit(1);
-   }
-
-   for(int j = 0; j < N_CARS; j++){
-      if((pipe(pipes[j])) != 0){
-          perror("Une erreur est survenue lors de l'envoi des invitations: ");
-          exit(1);
-      }
-   }
-   if((pipe(mainPipe)) != 0){
-       perror("Un hamster a retourné notre serveur mail, nous ne pouvons plus communiquer: ");
-       exit(1);
-   }
-
-   if ((memoire = shmat(mem_ID, NULL, 0)) == (void*) -1){
-       perror("Une erreur est survenue lors de l'assignation des places: ");
-       exit(1);
-   }
-
-   
-    
-   //TOUT LE STUFF EST A FAIRE ICI ! :D
-   
-   f1Car* voitures = (f1Car *)memoire;
-   
-   for(int i = 0; i < N_CARS; i++){
-       voitures[i].writingLock = 1;
-       voitures[i].carNumber = carNums[i];
-       voitures[i].writingLock = 0;
-   }
-   
-   *((f1Car*)memoire) = *voitures;
-   f1Car* nouvellesVoitures = (f1Car *)memoire;
-   f1Car laPremiereVoiture2 = voitures[0];
-   printf("%d\n", laPremiereVoiture2.carNumber);
-   
-   int pid;
-   int nProc;
-   for(nProc = 0; nProc < N_CARS; nProc++){
-       pid = fork();
-       if(pid == 0){
-           break;
-       }
-   }
-
-   if(pid == 0){
-        int memSlot = 0;
-        int writingPipe = 0;
-       
-        f1Car* mem = (f1Car *)memoire;
-        while(memSlot < N_CARS && mem[memSlot].writingLock == 1){
-            memSlot++;
-        }
-        mem[memSlot].writingLock = 1;
-        printf("%d\n", memSlot);
-        
-        close(pipes[memSlot][1]);
-        close(mainPipe[0]);
-        int writeTo = mainPipe[1];
-        int readFrom = pipes[memSlot][0];      
-       
-        if(memSlot == 21){
-            //Je sais que je suis le dernier, je vais donc prévenir tout le monde
-            while(memSlot > 0){
-                mem[memSlot--].writingLock = 0;
-            }  
-            f1CarEvent readyEvent;
-            readyEvent.carCode = 21;
-            readyEvent.eventCode = 0;
-            write(writeTo, &readyEvent, sizeof(f1CarEvent));
-        }
-    }else{
-        //close(pipes[memSlot][1]);
-        close(mainPipe[1]);
-        int readFrom = mainPipe[0];
-        int writeTo[22];
-        for(int d = 0; d < N_CARS; d++){
-            close(pipes[d][0]);
-            writeTo[d] = pipes[d][1];
-        }
-        while(!mustDie){
-            //Read pipe loop
-            //GUI
-            //Response
-            //A un moment ou un autre...
-            mustDie = 1;
-        }                
-
-
-        //Je suis ton pere!
-//           _________
-//           III| |III
-//         IIIII| |IIIII
-//        IIIIII| |IIIIII
-//        IIIIII| |IIIIII
-//       IIIIIII| |IIIIIII
-//       IIIIIII\ /IIIIIII
-//      II (___)| |(___) II
-//     II  \    /D\    /  II
-//    II  \ \  /| |\  / /  II
-//   II    \_\/|| ||\/_/    II
-//  II     / O-------O \     II
-// II_____/   \|||||/   \_____II
-//       /               \
-//       |_______________|
-   }
-    
-   shmdt(memoire);
-   shmctl(mem_ID, IPC_RMID, NULL);
+   int cars[22] = {44, 6, 5, 7, 3, 33, 19, 77, 11, 27, 26, 55, 14, 22, 9, 12, 20, 30, 8, 21, 31, 94}; // thx nico :D
 }
