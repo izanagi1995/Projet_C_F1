746555cae8d9c837b11e0f7a6e7cb343304a9685
Julien00859 <julien.castiaux@gmail.com>
2016-10-09 03:51:55 +0200

    Init code: msgq, shm, fork and some others shit...

--- defs.h
+++ defs.h
@@ -1,11 +1,8 @@
-#define MEM_KEY 20135
-
 // Core status
 #define S_INIT				0
-#define S_READY				1
-#define S_TEST_RACES		2
-#define S_QUALIFYING_RACES	3
-#define S_RACE				4
+#define S_TEST_RACES		1
+#define S_QUALIFYING_RACES	2
+#define S_RACE				3
 
 // Times for test races, qualif races and the race
 #define T_T1	90
@@ -26,19 +23,22 @@ struct sector_times {
 	float s3;
 };
 
-/* all info for a pilote */
-typedef struct f1_pilote f1_pilote;
-struct f1_pilote {
-	unsigned int id; // internaly used id [0; #PILOTES]
-	unsigned int pilote_id; // printable id (pilote number)
-	void* current_lap; // point a sector_times
-	char current_sector; // sector [1], sector [2], sector [3]
-	char status; // [D]riving, [P]itstop, [O]ut
-};
-
 /* store the time for each pilote at the end of a race */
 typedef struct stored_time stored_time;
 struct stored_time {
 	unsigned int pilote_id;
-	void* first_lap;
+	sector_times *first_lap;
+};
+
+/* all info for a pilote */
+typedef struct f1_pilote f1_pilote;
+struct f1_pilote {
+	unsigned int pilote_id;
+	struct times {
+		float s1;
+		float s2;
+		float s3;
+	} times; // point a sector_times
+	char current_sector; // sector [1], sector [2], sector [3]
+	char status; // [D]riving, [P]itstop, [O]ut
 };
--- main.c
+++ main.c
@@ -3,21 +3,89 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
+#include <sys/msg.h>
 #include <sys/shm.h>
+#include <time.h>
+#include <errno.h>
+#include <limits.h>
+#include <boolean.h>
 #include "defs.h"
+#include "tools.c"
 
-/* Check the return value of a syscall, on fail value print a message to stderr and exit, on success value return the value
-	@param call_return: value return by the syscall
-	@param msg_on_fail: message to print before errno if syscall failed
-	@return: call_return
-	@throws: EXIT_FAILURE
-*/
-int try_sys_call(int call_return, char msg_on_fail[]) {
-	if (call_return >= 0) return call_return;
-	perror(msg_on_fail);
-	exit(EXIT_FAILURE);
+
+volatile sig_atomic_t flag = 0;
+void sig_handler(int sig) {
+	flag = 1;
 }
 
 int main(int argc, char *argv[]){
-   int cars[22] = {44, 6, 5, 7, 3, 33, 19, 77, 11, 27, 26, 55, 14, 22, 9, 12, 20, 30, 8, 21, 31, 94}; // thx nico :D
+	int status = S_INIT;
+
+	srand(time(NULL));
+	int i;
+
+	int cars[22] = {44, 6, 5, 7, 3, 33, 19, 77, 11, 27, 26, 55, 14, 22, 9, 12, 20, 30, 8, 21, 31, 94};
+	int cars_length = sizeof(cars) / sizeof(int); 
+
+	int msgqkey;
+	int msgqid;
+	do {
+		msgqkey = rand();
+		msgqid = msgget(rand(), IPC_CREAT | IPC_EXCL | 0660);
+	} while (errno == EEXIST)
+	msgqid = try_sys_call_int(msgqid, "msgget failure");
+	msgbuf buffer;
+
+	int pid;
+	int shm_key;
+	int temp_id;
+	for (i = 0; i < cars_length; ++i) {
+		do {
+			shm_key = rand();
+			temp_id = shmget(shm_key, sizeof(f1_pilote), IPC_CREAT | IPC_EXCL | 0660);
+		} while (errno == EEXIST);
+		pid = try_sys_call_int(fork(), "fork failure");
+		if (pid > 0) break;
+	}
+
+	if (pid > 0) {
+		/* Register pid of child*/
+		int *pids = malloc(sizeof(int) * cars_length);
+
+		// read pids from message queue, not saved before for security purpose
+		// send a signal to all children when all pid have been recieved
+
+		/* Init memory sharing
+		%2		=> mem_id given by shmget()
+		%2 + 1	=> att_id given by shmat()*/
+		int *shmems = malloc(sizeof(int) * cars_length * 2);
+
+		// read keys from messages queue, not saved before for security purpose
+		// set the status on S_TEST_RACES and send a signal to children
+
+	} else {
+
+
+		int shmem = try_sys_call_int(shmatt(shm_key, NULL, 0), "shmatt failure");
+		pid = getpid();
+
+		char *pid_as_char = (char *) malloc(sizeof(pid_t) * sizeof(char)); // fuck magic numbers :DDD
+		sprintf(pid_as_char, "%d", (int)pid);
+		buffer = {1, pid_as_char}
+
+		msgsnd(msgqid, buffer, sizeof(pid_t), 0);
+
+		while(true) if (flag == 1) break;
+		flag = 0;
+
+		char *shmkey_as_char = (char *) malloc(sizeof(int) * sizeof(char));
+		sprintf(shmkey_as_char, "%d", shm_key);
+		buffer = {2, shmkey_as_char}
+		msgsnd(msgqid, buffer, sizeof(int))
+
+		while(true) if (flag == 1) break;
+		flag = 0;
+
+		// S_TEST_RACE
+	}
 }
